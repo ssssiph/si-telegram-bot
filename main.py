80d1902e1d4cd356ca71ae2148d466304deb58e5
ssssiph <alt.siph@gmail.com>
2025-05-18 10:23:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,13 +6,11 @@ from aiogram.client.default import DefaultBotProperties
 from aiogram.fsm.storage.memory import MemoryStorage
 from dotenv import load_dotenv
 
-from database import init_db, get_connection
+from database import init_db
 from handlers import start, account, events, contact, manage
 
 load_dotenv()
-
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-DATABASE_URL = os.getenv("DATABASE_URL")
 
 bot = Bot(
     token=BOT_TOKEN,
@@ -29,27 +27,8 @@ dp.include_routers(
     manage.router
 )
 
-async def ensure_director():
-    conn = await get_connection()
-    try:
-        tg_id = 1016554091
-        exists = await conn.fetchrow("SELECT * FROM users WHERE tg_id = $1", tg_id)
-        if not exists:
-            await conn.execute(
-                "INSERT INTO users (tg_id, username, rank, balance) VALUES ($1, $2, 'Генеральный директор', 0)",
-                tg_id, 'siph_director'
-            )
-        else:
-            await conn.execute(
-                "UPDATE users SET rank = 'Генеральный директор' WHERE tg_id = $1",
-                tg_id
-            )
-    finally:
-        await conn.close()
-
 async def main():
     await init_db()
-    await ensure_director()
     await dp.start_polling(bot)
 
 if __name__ == "__main__":

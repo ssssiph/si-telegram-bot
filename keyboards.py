5c970e7889e4d4faf4c60fe261b6d28361d50436
ssssiph <alt.siph@gmail.com>
2025-05-18 12:15:28 +0300

    Update keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -1,10 +1,16 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
-main_menu = ReplyKeyboardMarkup(keyboard=[
-    [KeyboardButton(text="👤 Аккаунт"), KeyboardButton(text="🎯 События")],
-    [KeyboardButton(text="📩 Связь"), KeyboardButton(text="🛠 Управление")]
-], resize_keyboard=True)
+main_menu = ReplyKeyboardMarkup(
+    keyboard=[
+        [KeyboardButton(text="👤 Аккаунт"), KeyboardButton(text="🎯 События")],
+        [KeyboardButton(text="📩 Связь"), KeyboardButton(text="🛠 Управление")]
+    ],
+    resize_keyboard=True
+)
 
-back_menu = ReplyKeyboardMarkup(keyboard=[
-    [KeyboardButton(text="◀️ Назад")]
-], resize_keyboard=True)
+back_menu = ReplyKeyboardMarkup(
+    keyboard=[
+        [KeyboardButton(text="◀️ Назад")]
+    ],
+    resize_keyboard=True
+)

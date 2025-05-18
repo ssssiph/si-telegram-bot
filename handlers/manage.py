18461042aa3d87da6ee1659572224265997013aa
ssssiph <alt.siph@gmail.com>
2025-05-18 09:14:30 +0300

    Update manage.py

--- handlers/manage.py
+++ handlers/manage.py
@@ -6,14 +6,14 @@ from fsm.create_event import CreateEvent
 
 router = Router()
 
-@router.message(F.text == "🛠 Управление")
+@router.message(F.text.strip() == "🛠 Управление")
 async def admin_panel(message: Message, state: FSMContext):
     await message.answer(
         "🛠 Панель управления:\n\n1. Создать событие",
         reply_markup=back_menu
     )
 
-@router.message(F.text == "1")
+@router.message(F.text.strip() == "1")
 async def start_event_creation(message: Message, state: FSMContext):
     await state.set_state(CreateEvent.title)
     await message.answer("Введите <b>название</b> события:")

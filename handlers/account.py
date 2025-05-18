8c6ac8f6e62db22d665ea84661e656a151372b8c
ssssiph <alt.siph@gmail.com>
2025-05-18 12:47:09 +0300

    Update account.py

--- handlers/account.py
+++ handlers/account.py
@@ -6,13 +6,14 @@ router = Router()
 
 @router.message(lambda message: message.text is not None and message.text.strip() == "👤 Аккаунт")
 async def account_info(message: Message):
+    print(f"[ACCOUNT] Получили команду от пользователя {message.from_user.id}: {message.text}")
     conn = await get_connection()
     try:
         user = await conn.fetchrow("SELECT * FROM users WHERE tg_id = $1", message.from_user.id)
         if not user:
-            await message.answer("❗ Пользователь не найден. Отправьте /start для регистрации.")
+            await message.answer("❗ Пользователь не найден. Пожалуйста, отправьте /start для регистрации.")
+            print(f"[ACCOUNT] Пользователь {message.from_user.id} не зарегистрирован!")
             return
-
         await message.answer(
             f"<b>🧾 Ваш аккаунт:</b>\n"
             f"ID: <code>{user['tg_id']}</code>\n"
@@ -21,5 +22,6 @@ async def account_info(message: Message):
             f"Ранг: {user['rank']}\n"
             f"💎 Баланс: {user['balance']}"
         )
+        print(f"[ACCOUNT] Информация для пользователя {message.from_user.id} отправлена.")
     finally:
         await conn.close()

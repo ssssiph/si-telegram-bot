2adbfb57f3258abd226861f108e9883938629f35
ssssiph <alt.siph@gmail.com>
2025-05-18 13:56:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import asyncio
 import os
 from aiogram import Bot, Dispatcher
+from aiogram.enums import ParseMode
+from aiogram.client.default import DefaultBotProperties
 from aiogram.fsm.storage.memory import MemoryStorage
 from dotenv import load_dotenv
 
@@ -10,7 +12,7 @@ from handlers import start, account, events, contact, manage
 load_dotenv()
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
-bot = Bot(token=BOT_TOKEN, parse_mode="HTML")
+bot = Bot(token=BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
 dp = Dispatcher(storage=MemoryStorage())
 
 dp.include_routers(
@@ -25,5 +27,5 @@ async def main():
     await init_db()
     await dp.start_polling(bot)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     asyncio.run(main())

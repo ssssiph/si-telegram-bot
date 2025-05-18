ec603d4129ad0781857409454ca4b38cdf544bf8
ssssiph <alt.siph@gmail.com>
2025-05-18 12:01:29 +0300

    Update manage.py

--- handlers/manage.py
+++ handlers/manage.py
@@ -13,14 +13,15 @@ async def admin_panel(message: Message):
 
         if not user:
             await conn.execute(
-                "INSERT INTO users (tg_id, username, full_name, rank, balance) VALUES ($1, $2, $3, 'Гость', 0)",
+                """
+                INSERT INTO users (tg_id, username, full_name, rank, balance)
+                VALUES ($1, $2, $3, 'Гость', 0)
+                """,
                 message.from_user.id,
                 message.from_user.username or "-",
                 message.from_user.full_name or "-"
             )
-            user = {
-                "rank": "Гость"
-            }
+            user = {"rank": "Гость"}
 
         if user["rank"] != "Генеральный директор":
             await message.answer("❌ У вас нет доступа к управлению.")

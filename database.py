c0e6a101e3d4024995736b7ac95091662e17d832
ssssiph <alt.siph@gmail.com>
2025-05-18 08:36:08 +0300

    Update database.py

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-DATABASE_URL = os.getenv("DATABASE_URL").replace("postgresql://", "postgresql+asyncpg://")
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 async def get_connection():
     return await asyncpg.connect(DATABASE_URL)

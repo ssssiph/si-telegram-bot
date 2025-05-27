757d6a2cd0fc4f1a365a83e5bd890fb43307223d
ssssiph <alt.siph@gmail.com>
2025-05-27 22:41:37 +0300

    Update account.py

--- handlers/account.py
+++ handlers/account.py
@@ -12,19 +12,24 @@ async def account_info(message: Message):
             await cur.execute("SELECT * FROM users WHERE tg_id = %s", (message.from_user.id,))
             user = await cur.fetchone()
 
+            username = message.from_user.username or "-"
+            full_name = message.from_user.full_name or "-"
+
             if not user:
                 await cur.execute("""
                     INSERT INTO users (tg_id, username, full_name, `rank`, balance)
                     VALUES (%s, %s, %s, 'Гость', 0)
-                """, (
-                    message.from_user.id,
-                    message.from_user.username or "-",
-                    message.from_user.full_name or "-"
-                ))
+                """, (message.from_user.id, username, full_name))
                 await cur.execute("SELECT * FROM users WHERE tg_id = %s", (message.from_user.id,))
                 user = await cur.fetchone()
+            else:
+                await cur.execute("""
+                    UPDATE users SET username = %s, full_name = %s WHERE tg_id = %s
+                """, (username, full_name, message.from_user.id))
+                await conn.commit()
+                user = (message.from_user.id, username, full_name, user[3], user[4])
 
-            tg_id, username, full_name, rank, balance, *_ = user
+            tg_id, username, full_name, rank, balance = user[:5]
 
             await message.answer(
                 f"<b>🧾 Ваш аккаунт:</b>\n"
